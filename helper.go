5c2dd1569ff4614039737d061bfedea267d04177
Hanna <nguyen.thi.hanh@moneyforward.co.jp>
2021-11-18 14:22:12 +0700

    feat: calulate distance on maps

--- helper.go
+++ helper.go
@@ -1,6 +1,7 @@
 package pbl_otabe
 
 import (
+	"math"
 	"reflect"
 )
 
@@ -18,4 +19,28 @@ func ItemExists(arrayType interface{}, item interface{}) bool {
 	}
 
 	return false
+}
+
+// haversin(θ) function
+func Hsin(theta float64) float64 {
+	return math.Pow(math.Sin(theta/2), 2)
+}
+
+// distance returned is METERS!!!!!!
+// http://en.wikipedia.org/wiki/Haversine_formula
+func Distance(lat1, lon1, lat2, lon2 float64) float64 {
+	// convert to radians
+	// must cast radius as float to multiply later
+	var la1, lo1, la2, lo2, r float64
+	la1 = lat1 * math.Pi / 180
+	lo1 = lon1 * math.Pi / 180
+	la2 = lat2 * math.Pi / 180
+	lo2 = lon2 * math.Pi / 180
+
+	r = 6378100 // Earth radius in METERS
+
+	// calculate
+	h := Hsin(la2-la1) + math.Cos(la1)*math.Cos(la2)*Hsin(lo2-lo1)
+
+	return 2 * r * math.Asin(math.Sqrt(h))
 }
